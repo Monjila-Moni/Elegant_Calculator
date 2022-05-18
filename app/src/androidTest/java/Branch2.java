7cdc9e8b379dfd64fc584214d9e5e92c371eede3
MONI <moni1807112@stud.kuet.ac.bd>
2022-05-18 09:08:01 +0600

    conflicts create

--- app/src/androidTest/java/Branch2.java
+++ app/src/androidTest/java/Branch2.java
@@ -1,12 +1,12 @@
 
 public class Branch2 {
     String name,id;
-    float cgpa;
+
     Branch2(String s, String s1,float f)
     {
         this.name=s;
         this.id=s1;
-        this.cgpa=f;
+
     }
 
     public String getName() {
@@ -17,13 +17,7 @@ public class Branch2 {
         this.name = name;
     }
 
-    public float getCgpa() {
-        return cgpa;
-    }
 
-    public void setCgpa(float cgpa) {
-        this.cgpa = cgpa;
-    }
 
     public String getId() {
         return id;
