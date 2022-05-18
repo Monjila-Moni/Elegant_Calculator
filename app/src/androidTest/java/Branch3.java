e0eeb355a2004b0496ec1b96c3650aa5baae28c9
MONI <moni1807112@stud.kuet.ac.bd>
2022-05-18 08:52:20 +0600

    merage

---
+++ app/src/androidTest/java/Branch3.java
@@ -0,0 +1,35 @@
+public class Branch3 {
+        String name,id;
+        float cgpa;
+        Branch3(String s, String s1,float f)
+        {
+            this.name=s;
+            this.id=s1;
+            this.cgpa=f;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public float getCgpa() {
+            return cgpa;
+        }
+
+        public void setCgpa(float cgpa) {
+            this.cgpa = cgpa;
+        }
+
+        public String getId() {
+            return id;
+        }
+
+        public void setId(String id) {
+            this.id = id;
+        }
+    }
+}
